9d565d05f31c8bd98a7c1e24801af75b60cab761
vishal <vishalpalode9899@gmail.com>
2022-03-05 01:33:46 +0530

    [vishalpalode] added creat BST

--- BinarySearchTree/MyBinaryTree.cs
+++ BinarySearchTree/MyBinaryTree.cs
@@ -8,23 +8,20 @@ namespace BinarySearchTree
 {
     public class MyBinaryNode<T> where T : IComparable
     {
-<<<<<<< Updated upstream
-            T nodedata;
-            public MyBinaryNode<T> lefttree { get; set; }
-            public MyBinaryNode<T> righttree { get; set; }
-            bool result = false;
 
-            public MyBinaryNode(T nodedata)
-=======
         T nodedata;
         public MyBinaryNode<T> lefttree { get; set; }
         public MyBinaryNode<T> righttree { get; set; }
         bool result = false;
+
+
+
         static int leftcount = 0;
         static int rightcount = 0;
-        static MyBinaryNode() {
+        static MyBinaryNode()
+        {
             Console.WriteLine("static constructor");
-      
+
         }
         public MyBinaryNode(T nodedata)
         {
@@ -34,40 +31,30 @@ namespace BinarySearchTree
             this.lefttree = null;
             this.righttree = null;
         }
+
+
         public void Insert(T item)
         {
             T currentnodevalue = this.nodedata;
             if ((currentnodevalue.CompareTo(item)) > 0)
->>>>>>> Stashed changes
             {
-                this.nodedata = nodedata;
-                this.nodedata = nodedata;
-                this.lefttree = null;
-                this.righttree = null;
-            }
-
-            public void Insert(T item)
-            {
-                T currentnodevalue = this.nodedata;
-                if ((currentnodevalue.CompareTo(item)) > 0)
-                {
-                    if (this.lefttree == null)
-                        this.lefttree = new MyBinaryNode<T>(item);
-                    else
-                        this.lefttree.Insert(item);
-                }
+                if (this.lefttree == null)
+                    this.lefttree = new MyBinaryNode<T>(item);
                 else
-<<<<<<< Updated upstream
-                {
-                    if (this.righttree == null)
-                        this.righttree = new MyBinaryNode<T>(item);
-                    else
-                        this.righttree.Insert(item);
-                }
-=======
+                    this.lefttree.Insert(item);
+            }
+            else
+
+            {
+                if (this.righttree == null)
+                    this.righttree = new MyBinaryNode<T>(item);
+                else
                     this.righttree.Insert(item);
             }
+
+            this.righttree.Insert(item);
         }
+
         public void Display()
         {
             if (this.lefttree != null)
@@ -80,20 +67,8 @@ namespace BinarySearchTree
             {
                 rightcount++;
                 this.righttree.Display();
->>>>>>> Stashed changes
+
             }
-            public void Display()
-            {
-                if (this.lefttree != null)
-                {
-                    this.lefttree.Display();
-                }
-                Console.WriteLine(this.nodedata.ToString());
-                if (this.righttree != null)
-                {
-                    this.righttree.Display();
-                }
-            }
-        
+        }
     }
 }
